8d5201785a9e57adecb5acc971e1a21cc75b3e53
zhaojiedi <zhaojiedi1992@outlook.com>
2022-09-16 16:58:10 +0800

    rename

--- scripts/shop/generate_sg.sh
+++ scripts/shop/generate_sg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 file=sell.yml
-template_file="template.trmenu.sell.body.yml.template"
-head_file="template.trmenu.sell.head.yml.template"
+template_file="template.trmenu.sell.body.yml"
+head_file="template.trmenu.sell.head.yml"
 /usr/bin/cp $head_file $file
 cat sg.txt |grep -v "个数" |awk '{print $1,$2,$3,$4,$5,$6}'|while read name cname price left_cnt right_cnt sleft_cnt ; do 
 	echo "name=$name cname=$cname price=$price left_cnt=$left_cnt right_cnt=$right_cnt sleft_cnt=$sleft_cnt "
